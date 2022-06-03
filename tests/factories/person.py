31910fee809c6b8973e52fe2c8b70cd3380b2771
Yannick <ygosteli@gmail.com>
2022-06-03 21:01:58 +0200

    add person tests for 100% test coverage

---
+++ tests/factories/person.py
@@ -0,0 +1,21 @@
+import factory
+from factory import Sequence
+from factory.django import DjangoModelFactory
+
+from kikeou.models import Person
+from tests.factories.cycle import CycleFactory
+
+__all__ = ["PersonFactory"]
+
+
+class PersonFactory(DjangoModelFactory):
+    class Meta:
+        model = Person
+
+    cycle = factory.SubFactory(CycleFactory)
+    user = None
+    first_name = Sequence(lambda n: f"Toto-{n}")
+    last_name = Sequence(lambda n: f"Tutu-{n}")
+    phone = ""
+    email = ""
+    diet_type = ""
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from kikeou.models import Cycle, Person
+from tests.factories.person import PersonFactory
 from tests.utils.cycle import create_5_cycles_batch
 
 
@@ -37,3 +38,16 @@ class PersonTestCase(TestCase):
         person.last_name = "tutu"
         person.save()
         self.assertEqual(person.cycle.id, first_cycle.id)
+
+    def test_full_name_build(self):
+        self.assertEqual(
+            PersonFactory(first_name="Georges", last_name="Dupont").full_name,
+            "Georges Dupont",
+        )
+
+    def test_person_str_is_full_name(self):
+        person = PersonFactory(first_name="Georges", last_name="Dupont")
+        self.assertEqual(
+            str(person),
+            person.full_name,
+        )
